b8006466b85a9ee00e3df1bf9173614209e2dd7d
Michael Hallquist <michael.hallquist@gmail.com>
2016-11-15 17:50:40 -0500

    Further development of salience network simulation hammer.

--- setup_network_groundtruth.R
+++ setup_network_groundtruth.R
@@ -1,5 +1,6 @@
 #use 10895 data processed as part of possum motion
-setwd(file.path(getMainDir(), "clinical_network_sim"))
+library(dplyr)
+setwd(file.path(getMainDir(), "clinical_brainnetwork_sim"))
 source("simulate_clingraph.R")
 adjmat <- as.matrix(read.table("10895_bb264_corrmat.zval.1D"))
 mean(adjmat[lower.tri(adjmat)])
@@ -36,7 +37,7 @@ gmeasures <- function(g) {
   bet <- betweenness(g, normalize=TRUE)*100
   locclust <- transitivity(g, type="local")
   pr <- page.rank(g, algo="prpack")$vector
-  data.frame(degree=deg, evcent=evc, betweenness=bet, locclust=locclust, pagerank=pr) #return a 264 x nmetrics matrix for analysis/reduction            
+  data.frame(node=V(g)$name, degree=deg, evcent=evc, betweenness=bet, locclust=locclust, pagerank=pr) #return a 264 x nmetrics matrix for analysis/reduction            
 }
 
 #for basic checks, compute degree, eigenvector centrality, local clustering, betweenness, and pagerank at each density threshold
@@ -47,29 +48,174 @@ all_centrality <- lapply(allg_density, function(gthresh) {
 #number of edges
 sapply(allg_density, function(gthresh) { length(E(gthresh)) })
 
+#verify that functions work as expected
 sanitycheck_sims(g_positive)
 
-allsims <- simulate_clingraph(g_positive, npergroup=100, edgenoise=0.0, #just for sanity check
+
+testsim1 <- simulate_clingraph(g_positive, npergroup=100, edgenoise=0.05,
     groupmanipulation=list(
         controls=list(group_name="control", 
-            edgeshift_weight_mean_bw=0, edgeshift_weight_sd_wi=0, edgeshift_weight_sd_bw=0, #global shifts in weights
+            edgeshift_weight_mean_bw=0, edgeshift_weight_sd_wi=0, edgeshift_weight_sd_bw=0
+        ),         #global shifts in weights
+        patients=list(group_name="patient", edgeshift_weight_mean_bw=0.0, edgeshift_weight_sd_wi=0, edgeshift_weight_sd_bw=0, #global shifts in weights
             nodemanipulation=list( #nodal shifts in (weighted) edges incident to a node
-                list(name="V1", edgeshift_weight_mean_bw=20, edgeshift_weight_sd_wi=10, edgeshift_weight_sd_bw=5),
-                list(name="V189", edgeshift_weight_mean_bw=100, edgeshift_weight_sd_wi=10, edgeshift_weight_sd_bw=5)
+                list(name="V215", edgeshift_weight_mean_bw=.3, edgeshift_weight_sd_wi=.03, edgeshift_weight_sd_bw=.03), #D ACC
+                list(name="V208", edgeshift_weight_mean_bw=.4, edgeshift_weight_sd_wi=.04, edgeshift_weight_sd_bw=.04),  #L IFG
+                list(name="V209", edgeshift_weight_mean_bw=.4, edgeshift_weight_sd_wi=.04, edgeshift_weight_sd_bw=.04)   #R IFG
             )
-        ),
-        patients=list(group_name="patients", edgeshift_weight_mean_bw=0.5, edgeshift_weight_sd_wi=.06, edgeshift_weight_sd_bw=.25)))
+        )
+    )
+)
 
-#check on nodal tweaks to controls
-firstcontrol = allsims[["control"]][[1]]
-E(firstcontrol)[inc("V1")] #edges incident to vertex V1
-E(firstcontrol)[inc("V1")]$weight #edges weights incident to vertex V1 -- yup those are huge (given +20 mean)
-sd(E(firstcontrol)[inc("V1")]$weight) #wi node variation in strength for one replication
+#now look at between group differences
+#for a moment, just look at 10% density
+densities <- c(.05, .06, .07, .08, .09, .1)
+library(parallel)
+allg_density <- lapply(densities, function(d, simlist) {
+      lapply(simlist, function(group) {
+            mclapply(group, function(g) {
+                  #g is a replication igraph object
+                  #thresh <- quantile(E(g)$weight, 1-d) #this takes quantile of non-negative edges, not overall density based on graph diameter
+                  #gthresh <- delete.edges(g, which(E(g)$weight <= thresh)) #return binary weighted density-thresholded graph
+                  #gthresh <- remove.edge.attribute(gthresh, "weight")
+                  
+                  #related approach, but will obtain desired density given graph diameter
+                  weights <- sort(E(g)$weight, decreasing=TRUE)
+                  threshold <- weights[length(V(g))*(length(V(g))-1)/2 * d]
+                  gthresh <- delete.edges(g, which(E(g)$weight < threshold))
+                  gthresh <- remove.edge.attribute(gthresh, "weight")
+                  
+                  return(gthresh)
+                }, mc.cores=4)
+          })
+    }, testsim1)
 
-#verify that V1 -- V189 is not double tweaked (check the sequential processing of node tweaks): VERIFIED 30Sep2016
-E(firstcontrol)[inc("V189")] #which nodes are connected to 189 
-sort(E(firstcontrol)[inc("V189")]$weight) #edges incident to vertex V189 -- yup those are huge (given +100 mean)
-sd(E(firstcontrol)[inc("V1")]$weight) #wi node variation in strength for one replication
+names(allg_density) <- as.character(densities)
 
-E(firstcontrol)["V1" %--% "V189"]$weight #weight of the specific connection that should be weaker (~20-30) since V1 -- V189 was tweaked first
-firstcontrol["V1","V189"] #even more compact subset notation (igraph internally treats this as a weighted adjacency matrix) 
+x <- allg_density[["0.1"]][["control"]][[2]]
+graph.density(x)
+is.directed(x)
+
+#for basic checks, compute degree, eigenvector centrality, local clustering, betweenness, and pagerank at each density threshold
+all_centrality <- lapply(allg_density, function(dlist) {
+      lapply(dlist, function(group) {
+            mclapply(group, function(gthresh) {
+                  gmeasures(gthresh)            
+                })
+          })      
+    })
+
+str(all_centrality[[1]][["control"]][[1]])
+str(all_centrality[[1]][["patient"]][[1]])
+
+#compare target nodes
+subset(all_centrality[["0.05"]][["control"]][[1]], node %in% c("V207", "V208", "V209", "V215"))
+subset(all_centrality[["0.05"]][["patient"]][[1]], node %in% c("V207", "V208", "V209", "V215"))
+
+
+#get mean and sd centrality
+all_agg <- lapply(all_centrality, function(dlist) {
+      lapply(dlist, function(group) {
+            #here, group is a list of subjects where each is a 264 x measures data.frame
+            recode <- lapply(1:length(group), function(g) {
+                  group[[g]]$replication <- g #add replication
+                  group[[g]]$nodeNum <- 1:nrow(group[[g]]) #add replication
+                  return(group[[g]])
+                })            
+            df <- do.call(rbind, recode)
+            df %>% group_by(node) %>% summarize_at(vars(degree, evcent, betweenness, locclust, pagerank), funs(mean=mean, sd=sd))
+          })
+    })
+
+#take 0.1 for a sec
+xx <- all_centrality[["0.1"]]
+
+patients <- do.call(rbind, xx[["patient"]])
+patients$group <- "patient"
+controls <- do.call(rbind, xx[["control"]])
+controls$group <- "control"
+
+both <- rbind(patients, controls)
+
+library(broom)
+res <- lapply(split(both, both$node), function(df) {
+      if (sd(df$degree) < .01) { return(NULL)}
+      mm <- lm(degree ~ group, df)
+      partab <- tidy(mm)
+      pfilt <- filter(partab, term=="grouppatient") %>% mutate(node = df$node[1])
+      return(pfilt)
+    })
+
+resdf <- do.call(rbind, res)
+filter(resdf, estimate < 0)
+filter(resdf, estimate > 0)
+
+as_tibble(lapply(resdf, function(col) { 
+        if (is.numeric(col)) { return(round(col, 3))} else { return(col) }
+
+library(gridExtra)
+pdf("degreediff.pdf", height=11, width=8.5)
+grid.table(resdf[1:50,])
+dev.off()
+
+sum(filter(resdf, estimate < 0)$estimate) 
+sum(filter(resdf, estimate > 0)$estimate)
+
+
+library(broom)
+res <- lapply(split(both, both$node), function(df) {
+      if (sd(df$degree) < .01) { return(NULL)}
+      mm <- lm(evcent ~ group, df)
+      partab <- tidy(mm)
+      pfilt <- filter(partab, term=="grouppatient") %>% mutate(node = df$node[1])
+      return(pfilt)
+    })
+
+resdf <- do.call(rbind, res)
+filter(resdf, estimate < 0)
+filter(resdf, estimate > 0)
+
+sum(filter(resdf, estimate < 0)$estimate) 
+sum(filter(resdf, estimate > 0)$estimate)
+
+
+#now should be in a position to compare nodal statistics by group after thresholding
+d10 <- all_agg[["0.09"]]
+d10[["control"]]$group <- "control"
+d10[["patient"]]$group <- "patient"
+d10bind <- do.call(rbind, d10)
+
+
+
+library(ggplot2)
+pdf("deg diff test.pdf", width=12, height=40)
+ggplot(d10bind, aes(x=factor(node), y=degree_mean, ymin=degree_mean-degree_sd, ymax=degree_mean+degree_sd, fill=group)) + 
+    geom_bar(stat="identity", position="dodge", width=0.5) + 
+    geom_errorbar(position="dodge", width=0.5) +theme_bw(base_size=12) + coord_flip()
+    #facet_wrap(~node, ncol=1) + coord_flip() + theme_bw(base_size=12)
+dev.off()
+
+
+
+
+str(all_centrality[[1]][["control"]][[1]])
+
+
+
+#what edges are present at a given density for controls (unaltered) and patients (nodeal increases). Whack a node
+#plot these on brain
+
+#tr
+
+
+#test <- lapply(1:100,function(x) rnorm(100000))
+#system.time(x <- lapply(test,function(x) loess.smooth(x,x)))
+#system.time(x <- mclapply(test,function(x) loess.smooth(x,x), mc.cores=4))
+
+
+
+
+#global increase in edge strength in patients, then compare partial versus full correlation
+
+
+
--- simulate_clingraph.R
+++ simulate_clingraph.R
@@ -43,7 +43,7 @@ simulate_clingraph <- function(gobj, npergroup=100, groupmanipulation=NULL, edge
   #build sims
   for (g_iter in 1:length(groupmanipulation)) {
     #Gaussian variation in strength of shift across replications for a group
-    repshift <- groupmanipulation[[g_iter]]$edgeshift_weight_mean_bw + rnorm(npergroup, 0, groupmanipulation[[g_iter]]$edgeshift_weight_sd_bw) #mean shift + between-rep variation
+    repshift <- rnorm(npergroup, groupmanipulation[[g_iter]]$edgeshift_weight_mean_bw, groupmanipulation[[g_iter]]$edgeshift_weight_sd_bw) #mean shift + between-rep variation
     
     groupsims <- foreach(j = iter(1:npergroup), .packages="igraph") %dopar% {
       g_rep <- gobj
@@ -76,7 +76,7 @@ simulate_clingraph <- function(gobj, npergroup=100, groupmanipulation=NULL, edge
         node_edges <- difference(node_edges, edgesmanipulated) #pull out any edges already tweaked
         edgesmanipulated <- c(edgesmanipulated, node_edges) #add edges to edge sequence for this node
         
-        noderepshift <- node$edgeshift_weight_mean_bw + rnorm(npergroup, 0, node$edgeshift_weight_sd_bw) #mean shift + between-rep variation
+        noderepshift <- rnorm(npergroup, node$edgeshift_weight_mean_bw, node$edgeshift_weight_sd_bw) #mean shift + between-rep variation
         
         #tweak this node for each replication (loop over replications)
         groupsims <- lapply(1:npergroup, function(j) {
@@ -101,8 +101,9 @@ sanitycheck_sims <- function(g_positive) {
   edgeshift_weight_sd_wi=.06
   edgeshift_weight_mean_bw=0.5
   edgeshift_weight_sd_bw=.25
+  npergroup = 200
   
-  simlist <- simulate_clingraph(g_positive, npergroup=100, edgenoise=0.0, #need 0 edgenoise for sanity check
+  simlist <- simulate_clingraph(g_positive, npergroup=npergroup, edgenoise=0.0, #need 0 edgenoise for sanity check
       groupmanipulation=list(
           list(group_name="patients", edgeshift_weight_mean_bw=edgeshift_weight_mean_bw,
               edgeshift_weight_sd_wi=edgeshift_weight_sd_wi, edgeshift_weight_sd_bw=edgeshift_weight_sd_bw)))
@@ -134,7 +135,7 @@ sanitycheck_sims <- function(g_positive) {
   cat("Expected variation in shift across nodes (wi):", edgeshift_weight_sd_wi, "\n")
   cat("Observed variation:",   mean(repdevsds), "\n\n")
  
-  simlist <- simulate_clingraph(g_positive, npergroup=100, edgenoise=0.4) #no group structure
+  simlist <- simulate_clingraph(g_positive, npergroup=npergroup, edgenoise=0.4) #no group structure
   
   #convert to nconnections x nreps matrix
   weightmat <- sapply(simlist[["all"]], function(rep) { E(rep)$weight })
@@ -143,4 +144,90 @@ sanitycheck_sims <- function(g_positive) {
   
   cat("Expected edge noise (additive for each node): 0.4\n")
   cat("Observed edge noise:", mean(edgesds), "\n")
+  
+  #sanity check of nodal manipulations
+  v1edgeMean <- 20; v1edgeSDWi <- 7; v1edgeSDbw <- 5
+  V189edgeMean <- 100; V189edgeSDWi <- 25; V189edgeSDbw <- 5
+  
+  allsims <- simulate_clingraph(g_positive, npergroup=200, edgenoise=0.0, #just for sanity check
+      groupmanipulation=list(
+          controls=list(group_name="control", 
+              edgeshift_weight_mean_bw=0, edgeshift_weight_sd_wi=0, edgeshift_weight_sd_bw=0, #global shifts in weights
+              nodemanipulation=list( #nodal shifts in (weighted) edges incident to a node
+                  list(name="V1", edgeshift_weight_mean_bw=v1edgeMean, edgeshift_weight_sd_wi=v1edgeSDWi, edgeshift_weight_sd_bw=v1edgeSDbw),
+                  list(name="V189", edgeshift_weight_mean_bw=V189edgeMean, edgeshift_weight_sd_wi=V189edgeSDWi, edgeshift_weight_sd_bw=V189edgeSDbw)
+              )
+          ),
+          patients=list(group_name="patients", edgeshift_weight_mean_bw=0.5, edgeshift_weight_sd_wi=.06, edgeshift_weight_sd_bw=.25)))
+  
+  cat("\nShifting V1   by M =", v1edgeMean, ", SDwi =", v1edgeSDWi, ", SDbw=", v1edgeSDbw, "\n")
+  
+  controls <- allsims[["control"]]
+  
+  #per-replication average deviation between ground_truth and average weight
+  v1repdevmeans <- sapply(controls, function(replication) { mean(E(replication)[inc("V1")]$weight - E(g_positive)[inc("V1")]$weight) })
+  
+  cat("Expected mean shift in V1 edges: ", v1edgeMean, "\n")
+  cat("Observed mean shift in V1 edges across replications: ", mean(v1repdevmeans), "\n\n")
+  
+  #across-replication variation in mean shift (sd of mean shifts): should be close to edgeshift_weight_sd_bw (VERIFIED 30Sep2016)
+  cat("Expected variation in shift across replications (bw):", v1edgeSDbw, "\n")
+  cat("Observed variation:", sd(v1repdevmeans), "\n\n")
+  
+  #per-replication (within) edge variation from ground truth (across nodes)
+  repdevsds <- sapply(controls, function(replication) { sd(E(replication)[inc("V1")]$weight - E(g_positive)[inc("V1")]$weight) })
+  
+  #overall sd across nodes (within): this should be close to edgeshift_weight_sd_wi in the asymptote (VERIFIED 30Sep2016)
+  cat("Expected variation in shift across nodes (wi):", v1edgeSDWi, "\n")
+  cat("Observed variation:",   mean(repdevsds), "\n\n")
+  
+  cat("\nAfter V1, shifting V189 by M =", V189edgeMean, ", SDwi =", V189edgeSDWi, ", SDbw=", V189edgeSDbw, "\n")
+  
+  #per-replication average deviation between ground_truth and average weight
+  V189repdevmeans <- sapply(controls, function(replication) { mean(E(replication)[inc("V189")]$weight - E(g_positive)[inc("V189")]$weight) })
+  
+  cat("Expected mean shift in V189 edges: ", V189edgeMean, "\n")
+  cat("Observed mean shift in V189 edges across replications: ", mean(V189repdevmeans), "\n\n")
+  
+  #across-replication variation in mean shift (sd of mean shifts): should be close to edgeshift_weight_sd_bw (VERIFIED 30Sep2016)
+  cat("Expected variation in shift across replications (bw):", V189edgeSDbw, "\n")
+  cat("Observed variation:", sd(V189repdevmeans), "\n\n")
+  
+  #per-replication (within) edge variation from ground truth (across nodes)
+  V189repdevsds <- sapply(controls, function(replication) { sd(E(replication)[inc("V189")]$weight - E(g_positive)[inc("V189")]$weight) })
+  
+  #overall sd across nodes (within): this should be close to edgeshift_weight_sd_wi in the asymptote (VERIFIED 30Sep2016)
+  cat("Expected variation in shift across nodes (wi):", V189edgeSDWi, "\n")
+  cat("Observed variation:",   mean(V189repdevsds), "\n\n")
+  
+  cat("Checking that the V1 -- V189 connection is not dialed up to the V189 level since it was tweaked first in the sequence\n")
+  cat("Change should be something on the order of 20-30, not 100-150\n")
+  
+  v1_V189 <- mean(sapply(controls, function(replication) { replication["V1","V189"] })) #even more compact subset notation (igraph internally treats this as a weighted adjacency matrix)
+  cat("Observed mean connection between V1 and V189: ", v1_V189, "\n\n")
+  
+  #some leftover checks
+#  v1edgeReps <- do.call(rbind, lapply(controls, function(g) { E(g)[inc("V1")]$weight}))
+#  v1edgeMeans <- colMeans(v1edgeReps)
+#  colSDs()
+#  
+#  
+##check on nodal tweaks to controls
+#  firstcontrol = allsims[["control"]][[1]]
+#  E(firstcontrol)[inc("V1")] #edges incident to vertex V1
+#  E(firstcontrol)[inc("V1")]$weight #edges weights incident to vertex V1 -- yup those are huge (given +20 mean)
+#  sd(E(firstcontrol)[inc("V1")]$weight) #wi node variation in strength for one replication: Should match edgeshift_weight_sd_wi
+#  
+##verify that V1 -- V189 is not double tweaked (check the sequential processing of node tweaks): VERIFIED 30Sep2016
+#  E(firstcontrol)[inc("V189")] #which nodes are connected to 189 
+#  sort(E(firstcontrol)[inc("V189")]$weight) #edges incident to vertex V189 -- yup those are huge (given +100 mean)
+#  E(firstcontrol)[inc("V189")]$weight #edges incident to vertex V189 -- yup those are huge (given +100 mean)
+#  sd(E(firstcontrol)[inc("V1")]$weight) #SD of edges incident to V1. should correspond to edgeshift_weight_sd_wi for V1
+#  sd(E(firstcontrol)[inc("V189")]$weight) #wi node variation in strength for one replication. should correspond to edgeshift_weight_sd_wi for V2
+#  
+#  E(firstcontrol)["V1" %--% "V189"]$weight #weight of the specific connection that should be weaker (~20-30) since V1 -- V189 was tweaked first
+#  firstcontrol["V1","V189"] #even more compact subset notation (igraph internally treats this as a weighted adjacency matrix) 
+#  g_positive["V1","V189"] #even more compact subset notation (igraph internally treats this as a weighted adjacency matrix) 
+#  
+  
 }
